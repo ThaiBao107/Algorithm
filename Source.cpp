cc08ce6b21b82df1327e268a38c35e4b89a97dc7
Peter <127222462+ThaiBao107@users.noreply.github.com>
2024-01-03 09:08:31 +0700

    Kruskal_Algotithm

--- Source.cpp
+++ Source.cpp
@@ -1,185 +1,147 @@
 #include<iostream>
 #include<fstream>
+#define MAX 100
+
 using namespace std;
-#define MAX 20
-
-struct NODE
-{
-	int data;
-	NODE* link;
-};
-
-
-struct Queue
-{
-	NODE* first;
-	NODE* last;
-};
-
-
-NODE* init_NODE(int x)
-{
-	NODE* p = new NODE;
-	p->data = x;
-	p->link = NULL;
-	return p;
-}
-
-
-void init_Queue(Queue &q)
-{
-	q.first = NULL;
-	q.last = NULL;
-}
-
-
-void Push(Queue& q, NODE* p)
-{
-	if (q.first == NULL)
-		q.first = q.last = p;
-	else
-	{
-		q.last->link = p;
-		q.last = p;
-	}
-}
-
-
-void Pop(Queue& q, int& x)
-{
-	if (q.first == NULL)
-		return;
-	else
-	{
-		x = q.first->data;
-		NODE* h = q.first;
-		q.first = q.first->link;
-		delete h;
-	}
-}
-
-
-int read_file_SL()
-{
-	int n = 0;
-	ifstream inF;
-	inF.open("MTK.txt", ios_base::in);
-	if (inF.is_open())
-	{
-		char b;
-		do
-		{
-			inF >> b;
-			n++;
-		} while (b != '0' && b != '1');
-		inF.close();
-	}
-	else
-		cout << "khong mo duoc file" << endl;
-	return n - 1;
-}
-
 
 void read_file(int a[][MAX], int& n)
 {
 	ifstream inF;
-	inF.open("MTK.txt", ios_base::in);
+	inF.open("data.txt", ios_base::in);
 	if (inF.is_open())
 	{
-		char c; int b;
-		n = read_file_SL();
+		int b;
+		inF >> n;
 		for (int i = 0; i < n; i++)
-			inF >> c;
-
-		while (!inF.eof())
-		{
-			for(int i = 0;i<n;i++)
-				for (int j = 0; j < n; j++)
-				{
-					inF >> b;
-					a[i][j] = b;
-				}
-		}
+			for (int j = 0; j < n; j++)
+			{
+				inF >> b;
+				a[i][j] = b;
+			}
 		inF.close();
-		cout << "Doc file thanh cong" << endl;
 	}
 	else
-		cout << "khong doc duoc file" << endl;
+		cout << "Khong mo duoc file" << endl;
+
 }
 
 
-void show_graph(int a[][MAX], int n)
+
+void duyet_mang(int a[][MAX], int n)
 {
+	cout << "Danh sach" << endl;
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < n; j++)
 			cout << a[i][j] << " ";
 		cout << endl;
-	}		
-}
-
-
-void init_dinhchuaxet(int C[], int n)
-{
-	for (int i = 0; i < n; i++)
-		C[i] = 1;
-}
-
-int bfs[MAX];
-
-void Browse_BFS(int a[][MAX], int n, int k)
-{
-	int C[MAX];
-	int count_bfs = 0;
-	Queue q;
-	init_Queue(q);
-	init_dinhchuaxet(C, n);
-	NODE* p = init_NODE(k);
-	Push(q, p);
-	C[k] = 0;
-	while (q.first != NULL)
-	{
-		int x;
-		Pop(q, x);
-		bfs[count_bfs++] = x;
-		for (int i = 0; i< n; i++)
-			if (C[i] == 1 && a[x][i] == 1)
-			{
-				NODE* b = init_NODE(i);
-				Push(q, b);
-				C[i] = 0;
-			}
 	}
 }
 
 
-void browse_graph(int n)
+
+
+void TaoE(int E1[], int E2[], int wE[], int& nE, int a[][MAX], int n)
 {
 	for (int i = 0; i < n; i++)
-		cout << bfs[i] << " ";
+		for (int j = 0; j < n; j++)
+		{
+			if (a[i][j] != 0)
+			{
+				E1[nE] = i;
+				E2[nE] = j;
+				wE[nE] = a[i][j];
+				a[i][j] = 0;
+				a[j][i] = 0;
+				nE++;
+			}
+		}
 }
+
+
+
+
+void swap(int& a, int& b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+
+
+
+void sapxep(int E1[], int E2[], int wE[], int nE)
+{
+	for (int i = 0; i < nE - 1; i++)
+		for (int j = i + 1; j < nE; j++)
+			if (wE[i] > wE[j])
+			{
+				swap(E1[i], E1[j]);
+				swap(E2[i], E2[j]);
+				swap(wE[i], wE[j]);
+			}
+}
+
+int TonTai(int D[], int nT, int k)
+{
+	for (int i = 0; i < nT; i++)
+		if (D[i] == k) return 1;
+	return 0;
+}
+
+
+void Kruskal(int E1[], int E2[], int wE[], int& nE, int T1[], int T2[], int wT[], int& nT, int a[][MAX], int& n)
+{
+	TaoE(E1, E2, wE, nE, a, n);
+	sapxep(E1, E2, wE, nE);
+	for (int i = 0; i < nE; i++)
+	{
+		if (TonTai(T1, nT, E1[i]) == 1 && TonTai(T2, nT, E2[i]) == 1) continue;
+		if (TonTai(T1, nT, E2[i]) == 1 && TonTai(T2, nT, E1[i]) == 1) continue;
+		T1[nT] = E1[i];		
+		T2[nT] = E2[i];
+		wT[nT] = wE[i];
+		nT++;
+		if (nT == n - 1) break;
+	}
+}
+
+
+void duyet_Kruskal(int T1[], int T2[], int wT[], int nT)
+{
+	int tong = 0;
+	cout << "Kruskal" << endl;
+	for (int i = 0; i < nT; i++)
+	{
+		cout << "(" << T1[i] << "," << T2[i] << "): " << wT[i] << endl;
+		tong += wT[i];
+	}
+	cout << "Tong: " << tong << endl;
+}
+
 
 
 int main()
 {
-	int n = 0;
-	int a[MAX][MAX];
 	int choose;
+	int a[MAX][MAX];
+	int n = 0;
 	do
 	{
-		cout << "--------MENU--------------" << endl;
-		cout << "1. Read file                " << endl;
-		cout << "2. Browse by array             " << endl;
-		cout << "3. Browse by BFS                 " << endl;
-		cout << "4. EXIT                           " << endl;
-		cout << "--------------------------" << endl;
+		cout << "---------------MENU----------------------" << endl;
+		cout << "1. Doc file                               " << endl;
+		cout << "2. Show mang                               " << endl;
+		cout << "3. Kruskal                                " << endl;
+		cout << "4. EXIT                                   " << endl;
+		cout << "-----------------------------------------" << endl;
 		cout << "Choose: ";
 		while (true)
 		{
 			cin >> choose;
 			if (choose > 0 && choose <= 4) break;
 			else
-				cout << "Nhap sai, nhap lai: ";
+				cout << "Nhap sai yeu cua, nhap lai: ";
 		}
 		system("cls");
 		switch (choose)
@@ -187,39 +149,29 @@ int main()
 		case 1:
 		{
 			read_file(a, n);
+			cout << "Doc file thanh cong" << endl;
 			break;
 		}
 		case 2:
 		{
-			if (n == 0)
-				cout << "Chua co do thi" << endl;
+			if (n == 0) cout << "Chua co du lieu" << endl;
 			else
 			{
-				show_graph(a, n);
+				duyet_mang(a, n);
 			}
 			break;
 		}
 		case 3:
 		{
-			if (n == 0)
-				cout << "Chua co do thi" << endl;
+			if (n == 0) cout << "Chua co du lieu" << endl;
 			else
 			{
-				int l;
-				cout << "Nhap dinh nat dau duyet: ";
-				while (true)
-				{
-					cin >> l;
-					if (l >= 0 && l <= n) break;
-					else
-						cout << "Khong co dinh da nhap" << endl;
-				}
-				system("cls");
-				Browse_BFS(a, n, l);
-				cout << "Duyet thanh cong" << endl;
-				system("pause");
-				system("cls");
-				browse_graph(n);
+				int E1[MAX];        int T1[MAX];
+				int E2[MAX];        int T2[MAX];
+				int wE[MAX];        int wT[MAX];
+				int nE = 0;			int nT = 0;
+				Kruskal(E1, E2, wE, nE, T1, T2, wT, nT, a, n);
+				duyet_Kruskal(T1, T2, wT, nT);
 			}
 			break;
 		}
@@ -232,4 +184,3 @@ int main()
 		system("cls");
 	} while (true);
 }
-
